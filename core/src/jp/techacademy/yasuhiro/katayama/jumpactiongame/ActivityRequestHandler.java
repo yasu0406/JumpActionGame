4c1a807b900e2276ac08e02c3d1126a8158a8da1
katayama <yasucom4646@gmail.com>
2017-01-26 22:58:43 +0900

    Initial commit

---
+++ core/src/jp/techacademy/yasuhiro/katayama/jumpactiongame/ActivityRequestHandler.java
@@ -0,0 +1,9 @@
+package jp.techacademy.yasuhiro.katayama.jumpactiongame;
+
+/**
+ * Created by katayama on 2017/01/26.
+ */
+
+public interface ActivityRequestHandler {
+    public void showAds(boolean show);
+}
